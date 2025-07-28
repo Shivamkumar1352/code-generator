0a1fe8a70ea099860fbbb28b513f00b764b34ab5
Shivamkumar1352 <shivamkumar1352002@gmail.com>
2025-07-29 00:14:01 +0530

    update

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,4 +1,4 @@
-const UserModel = require.cache[require.resolve('../models/User')]?.exports || require('../models/User');
+const UserModel = require.cache[require.resolve('../Models/User')]?.exports || require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
